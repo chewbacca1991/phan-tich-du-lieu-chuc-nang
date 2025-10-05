622fd77ed336476cea574ec459c24d331e50aace
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-05 21:14:25 +0330

    Add a docstring to the MLModel class methods for better documentation.

--- machine_learning_model.py
+++ machine_learning_model.py
@@ -8,6 +8,11 @@ class MLModel:
         self.model = LinearRegression()
 
     def train(self):
+        """
+        Trains the linear regression model using the provided data.
+        Splits the data into training and test sets and fits the model.
+        Returns the score of the model on the test set.
+        """
         X = self.data.drop('target', axis=1)
         y = self.data['target']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -15,6 +20,11 @@ class MLModel:
         return self.model.score(X_test, y_test)
 
     def validate(self, new_data):
+        """
+        Validates the model using new data.
+        Raises a ValueError if 'target' column is missing in new_data.
+        Returns the score of the model on the new data set.
+        """
         if 'target' not in new_data.columns:
             raise ValueError("new_data must contain 'target' column.")
         X_new = new_data.drop('target', axis=1)
