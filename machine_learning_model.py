5921c961316ef512b0080d0e28b2213a600858a4
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-05 21:13:04 +0330

    Add validation method to MLModel for additional usability.

--- machine_learning_model.py
+++ machine_learning_model.py
@@ -13,3 +13,8 @@ class MLModel:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         self.model.fit(X_train, y_train)
         return self.model.score(X_test, y_test)
+
+    def validate(self, new_data):
+        X_new = new_data.drop('target', axis=1)
+        y_new = new_data['target']
+        return self.model.score(X_new, y_new)
