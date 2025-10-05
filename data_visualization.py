db57a13f6fb8419e35ffda41aace1c2608261003
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-05 21:13:39 +0330

    Update plot title for improved clarity

--- data_visualization.py
+++ data_visualization.py
@@ -7,7 +7,7 @@ class DataVisualizer:
     def plot_data(self):
         plt.figure(figsize=(10, 5))
         plt.plot(self.data)
-        plt.title('Data Analysis')  # Changed title for clarity
+        plt.title('Data Visualization Over Time')  # More descriptive title
         plt.xlabel('Time')
         plt.ylabel('Value')
         plt.show()
